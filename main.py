99f7ae0b96ecf634e8d0799b6560e35c1df27a7b
Roman L <r.lidyaev@yandex.ru>
2023-10-27 22:32:08 -0500

    move main to condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-from graphic_arts.start_game_banner import run_screensaver
+# from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -78,8 +78,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
-    run_screensaver()
+if __name__ == '__main__':
+    #run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,6 +88,4 @@ def main() -> None:
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
-
-main()
+    print(start_training(char_name, char_class)) 
